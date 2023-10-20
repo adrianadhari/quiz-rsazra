d1c75cd43ca433a3954a4422ac190aa3fbeed7f7
adrianadhari <adrianadhari@protonmail.com>
2023-10-20 21:19:51 +0700

    change config env

--- config/functions.php
+++ config/functions.php
@@ -1,8 +1,14 @@
 <?php
-$host     = "localhost";
-$username = "root";
-$password = "";
-$database = "db_quizazra";
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
+ 
+$host     = constant("DB_HOST");
+$username = constant("DB_USER");
+$password = constant("DB_PASS");;
+$database = constant("DB_NAME");
 $conn = mysqli_connect($host, $username, $password, $database);
 
 function register($data) {
