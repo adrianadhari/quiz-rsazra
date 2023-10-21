1091fb3eaed86cbe466019fa81af148bab34c1b1
adrianadhari <adrianadhari@protonmail.com>
2023-10-21 11:03:40 +0700

    apawe

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = getenv("MYSQL_SERVICE_HOST");
+$port = getenv("MYSQL_SERVICE_PORT");
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_NAME');
+
+$conn = mysqli_connect($host, $username, $password, $database);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
--- config/functions.php
+++ config/functions.php
@@ -1,13 +1,16 @@
 <?php
- 
-$host = getenv('DB_HOST');
-$port = getenv('DB_PORT');
+
+$host = getenv("MYSQL_SERVICE_HOST");
+$port = getenv("MYSQL_SERVICE_PORT");
 $username = getenv('DB_USERNAME');
 $password = getenv('DB_PASSWORD');
 $database = getenv('DB_NAME');
+
+
 $conn = mysqli_connect($host, $username, $password, $database);
 
-function register($data) {
+function register($data)
+{
     global $conn;
     $nama = stripslashes($data["nama"]);
     $username = strtolower(stripslashes($data["username"]));
@@ -21,9 +24,9 @@ function register($data) {
         echo '<div class="alert alert-warning" role="alert">
         <strong>Username yang anda daftarkan telah terdaftar, silahkan masukkan username yang lain.<strong>
       </div>';
-        return false;   
+        return false;
     }
-    if ( $password !== $password2) {
+    if ($password !== $password2) {
         echo '<div class="alert alert-warning" role="alert">
         <strong>Password yang anda masukan tidak sesuai</strong>
       </div>';
@@ -34,38 +37,42 @@ function register($data) {
     return mysqli_affected_rows($conn);
 }
 
-function deleteUser($username) {
+function deleteUser($username)
+{
     global $conn;
-         $d1 = mysqli_query($conn, "DELETE FROM tbl_akun WHERE username = '$username'");
-         $d2 = mysqli_query($conn, "DELETE FROM tbl_histori WHERE username = '$username'");
+    $d1 = mysqli_query($conn, "DELETE FROM tbl_akun WHERE username = '$username'");
+    $d2 = mysqli_query($conn, "DELETE FROM tbl_histori WHERE username = '$username'");
     return mysqli_affected_rows($conn);
 }
 
-function deleteQuiz($idquiz) {
+function deleteQuiz($idquiz)
+{
     global $conn;
-    $result = mysqli_query($conn,"SELECT * FROM tbl_pertanyaan WHERE idquiz='$idquiz'");
-    while($row = mysqli_fetch_array($result)) {
+    $result = mysqli_query($conn, "SELECT * FROM tbl_pertanyaan WHERE idquiz='$idquiz'");
+    while ($row = mysqli_fetch_array($result)) {
         $idpertanyaan = $row['idpertanyaan'];
         $r1 = mysqli_query($conn, "DELETE FROM tbl_pilihan WHERE idpertanyaan='$idpertanyaan'");
         $r2 = mysqli_query($conn, "DELETE FROM tbl_jawaban WHERE idpertanyaan='$idpertanyaan'");
     }
-        $r3 = mysqli_query($conn, "DELETE FROM tbl_pertanyaan WHERE idquiz='$idquiz'");
-        $r4 = mysqli_query($conn, "DELETE FROM tbl_quiz WHERE idquiz='$idquiz'");
-        $r4 = mysqli_query($conn, "DELETE FROM tbl_histori WHERE idquiz='$idquiz'");
+    $r3 = mysqli_query($conn, "DELETE FROM tbl_pertanyaan WHERE idquiz='$idquiz'");
+    $r4 = mysqli_query($conn, "DELETE FROM tbl_quiz WHERE idquiz='$idquiz'");
+    $r4 = mysqli_query($conn, "DELETE FROM tbl_histori WHERE idquiz='$idquiz'");
     return mysqli_affected_rows($conn);
 }
 
-function query($query) {
+function query($query)
+{
     global $conn;
     $result = mysqli_query($conn, $query);
     $rows = [];
     while ($fetch = mysqli_fetch_assoc($result)) {
-    $rows[] = $fetch;
+        $rows[] = $fetch;
     }
     return $rows;
 }
 
-function buatQuiz($data) {
+function buatQuiz($data)
+{
     global $conn;
     $judul = $data['judul'];
     $total = $data['total'];
@@ -76,41 +83,52 @@ function buatQuiz($data) {
     header("Location: ../dashboard/buatSoal.php?idquiz=$idquiz&n=$total");
 }
 
-function buatSoal($data) {
+function buatSoal($data)
+{
     global $conn;
-    $total=@$_GET['n'];
-    $idquiz=@$_GET['idquiz'];
-    $ch=@$_GET['ch'];
-    for($i=1;$i<=$total;$i++) {
-        $idpertanyaan=uniqid();
-        $pertanyaan=$data['pertanyaan'.$i];
-        $q3=mysqli_query($conn, "INSERT INTO tbl_pertanyaan VALUES ('$idquiz','$idpertanyaan','$pertanyaan' , '$ch' , '$i')");
-        $pilihanA=uniqid();
-        $pilihanB=uniqid();
-        $pilihanC=uniqid();
-        $pilihanD=uniqid();
-        $a=$data[$i.'1'];
-        $b=$data[$i.'2'];
-        $c=$data[$i.'3'];
-        $d=$data[$i.'4'];
-        $qa=mysqli_query($conn,"INSERT INTO tbl_pilihan VALUES ('$idpertanyaan','$a','$pilihanA')");
-        $qb=mysqli_query($conn,"INSERT INTO tbl_pilihan VALUES ('$idpertanyaan','$b','$pilihanB')");
-        $qc=mysqli_query($conn,"INSERT INTO tbl_pilihan VALUES ('$idpertanyaan','$c','$pilihanC')");
-        $qd=mysqli_query($conn,"INSERT INTO tbl_pilihan VALUES ('$idpertanyaan','$d','$pilihanD')");
-        $e=$data['jawaban'.$i];
-        switch($e) {
-          case 'a': $idjawaban=$pilihanA; break;
-          case 'b': $idjawaban=$pilihanB; break;
-          case 'c': $idjawaban=$pilihanC; break;
-          case 'd': $idjawaban=$pilihanD; break;
-          default: $idjawaban=$pilihanA;
+    $total = @$_GET['n'];
+    $idquiz = @$_GET['idquiz'];
+    $ch = @$_GET['ch'];
+    for ($i = 1; $i <= $total; $i++) {
+        $idpertanyaan = uniqid();
+        $pertanyaan = $data['pertanyaan' . $i];
+        $q3 = mysqli_query($conn, "INSERT INTO tbl_pertanyaan VALUES ('$idquiz','$idpertanyaan','$pertanyaan' , '$ch' , '$i')");
+        $pilihanA = uniqid();
+        $pilihanB = uniqid();
+        $pilihanC = uniqid();
+        $pilihanD = uniqid();
+        $a = $data[$i . '1'];
+        $b = $data[$i . '2'];
+        $c = $data[$i . '3'];
+        $d = $data[$i . '4'];
+        $qa = mysqli_query($conn, "INSERT INTO tbl_pilihan VALUES ('$idpertanyaan','$a','$pilihanA')");
+        $qb = mysqli_query($conn, "INSERT INTO tbl_pilihan VALUES ('$idpertanyaan','$b','$pilihanB')");
+        $qc = mysqli_query($conn, "INSERT INTO tbl_pilihan VALUES ('$idpertanyaan','$c','$pilihanC')");
+        $qd = mysqli_query($conn, "INSERT INTO tbl_pilihan VALUES ('$idpertanyaan','$d','$pilihanD')");
+        $e = $data['jawaban' . $i];
+        switch ($e) {
+            case 'a':
+                $idjawaban = $pilihanA;
+                break;
+            case 'b':
+                $idjawaban = $pilihanB;
+                break;
+            case 'c':
+                $idjawaban = $pilihanC;
+                break;
+            case 'd':
+                $idjawaban = $pilihanD;
+                break;
+            default:
+                $idjawaban = $pilihanA;
         }
-        $qans=mysqli_query($conn, "INSERT INTO tbl_jawaban VALUES  ('$idpertanyaan','$idjawaban')");
-      }
-      header("Location: ../dashboard/manageQuiz.php");
+        $qans = mysqli_query($conn, "INSERT INTO tbl_jawaban VALUES  ('$idpertanyaan','$idjawaban')");
+    }
+    header("Location: ../dashboard/manageQuiz.php");
 }
 
-function editQuiz($data) {
+function editQuiz($data)
+{
     global $conn;
     $idquiz = $data['idquiz'];
     $judul = $data['judul'];
@@ -118,7 +136,8 @@ function editQuiz($data) {
     return mysqli_affected_rows($conn);
 }
 
-function editUser($data) {
+function editUser($data)
+{
     global $conn;
     $id = $data['id'];
     $nama = stripslashes($data['nama']);
@@ -130,12 +149,13 @@ function editUser($data) {
     return mysqli_affected_rows($conn);
 }
 
-function editPassword($data) {
+function editPassword($data)
+{
     global $conn;
     $id = $data['id'];
     $password = mysqli_real_escape_string($conn, $data["password"]);
     $password2 = mysqli_real_escape_string($conn, $data["password2"]);
-    if ( $password !== $password2) {
+    if ($password !== $password2) {
         echo '<div class="alert alert-warning" role="alert">
         <strong>Password yang anda masukan tidak sesuai</strong>
       </div>';
@@ -145,4 +165,3 @@ function editPassword($data) {
     mysqli_query($conn, "UPDATE tbl_akun SET password = '$password' WHERE id = '$id'");
     return mysqli_affected_rows($conn);
 }
-?>
